1b675a728c7a2b2000b44d277349d05f4ce7e3c4
Hazem Abolila <hazemabolila44@gmail.com>
2023-03-17 12:50:37 +0200

    The last digit example

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, dig);
 	}
-	else if (dig == 5)
+	else if (dig == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, dig);
 
