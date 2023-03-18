ef156132e43f740dc4dc3a5269456f709017adcd
Hazem Abolila <hazemabolila44@gmail.com>
2023-03-18 22:42:05 +0200

    12. Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 98; i++)
+	for (i = 0; i < 99; i++)
 	{
-		for (j = i + 1; j < 99; j++)
+		for (j = i + 1; j < 100; j++)
 		{
 			putchar((i / 10) + '0');
 			putchar((i % 10) + '0');
@@ -18,7 +18,7 @@ int main(void)
 			putchar((j / 10) + '0');
 			putchar((j % 10) + '0');
 			if (i == 98 && j == 99)
-				break;
+				continue;
 			putchar(',');
 			putchar(' ');
 		}
