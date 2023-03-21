c05359b256b71074b5a60b0fbbe6c198ffcadf3f
Hazem Abolila <hazemabolila44@gmail.com>
2023-03-21 23:36:08 +0200

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 10-main.c 10-add.c -o 10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Add 2 numbers
+ * @num1: The first number
+ * @num2: The second number
+ * Return: Sum of 2 numbers
+ *
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
