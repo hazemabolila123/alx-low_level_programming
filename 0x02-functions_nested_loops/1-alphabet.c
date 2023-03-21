26b6bb56a2b9dbc333160e61982c1f41bdd4d98b
Hazem Abolila <hazemabolila44@gmail.com>
2023-03-21 20:44:54 +0200

    Alphabet example

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - print all the chars
+ *
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; a <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+	return (0);
+}
