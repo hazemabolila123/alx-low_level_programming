5b9671b3290fd6d9f1dc0ba1d63439b933aca965
Hazem Abolila <hazemabolila44@gmail.com>
2023-03-23 17:00:36 +0200

    Triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_triangle - Fucnction display a triangle
+ * @size: Function parameters
+ *
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if(size <= 0)
+		_putchar('\n');
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = i; j < (size - 1); j++)
+		{
+			_putchar(' ');
+		}
+		for (j = 0; j <= i; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
