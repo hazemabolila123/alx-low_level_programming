2d2e0ccf31c4bcdb0a9c82463ce189f3d451ca24
Hazem Abolila <hazemabolila44@gmail.com>
2023-03-31 02:16:01 +0200

    Upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * string_toupper - Function
+ * @s: Functon parameter
+ * Return: Sucess
+ *
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0' ; i++)
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+			s[i] -= 32;
+	}
+	return (s);
+}
