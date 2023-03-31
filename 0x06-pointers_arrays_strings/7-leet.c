3171807f80ea913895a50f4c4eee4384d3ac76cf
Hazem Abolila <hazemabolila44@gmail.com>
2023-03-31 02:31:18 +0200

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+char *leet(char *s)
+{
+	int i, j;
+	char *c = "aAeEoOtTlL";
+	char *d = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; c[j] != '\0'; j++)
+		{
+			if (s[i] == c[j])
+				s[i] = d[j];
+		}
+	}
+	return (s);
+}
