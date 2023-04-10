8b7885f5077e6c0b8c9edc68d29863d44720caa6
Hazem Abolila <hazemabolila44@gmail.com>
2023-04-10 21:31:28 +0200

    To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Program start executing from main function
+ * @argc: Argument counter
+ * @argv: Argument values
+ * Return: Success
+ *
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	char *c;
+
+	while (--argc)
+	{
+		for (c = argv[argc]; *c; c++)
+		{
+			if (*c < '0' || *c > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			sum += atoi(argv[argc]);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
