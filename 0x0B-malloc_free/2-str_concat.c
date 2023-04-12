c62d2c2489ecde13c267c8c3ec7589d5aa76eaa2
Hazem Abolila <hazemabolila44@gmail.com>
2023-04-12 16:53:42 +0200

    He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,10 +13,8 @@ char *str_concat(char *s1, char *s2)
 	int i, cnt1 = 0, cnt2 = 0;
 	char *ptr;
 
-	if (s1 == NULL)
-		s1 = '\0';
-	if (s2 == NULL)
-		s2 = '\0';
+	if (s1 == NULL || s2 == NULL)
+		return (0);
 
 	for (i = 0; s1[i] != '\0'; i++)
 		cnt1++;
