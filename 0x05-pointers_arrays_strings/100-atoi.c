a33492b70a118a734c695f7f49c07876d0436d6f
Hazem Abolila <hazemabolila44@gmail.com>
2023-04-15 17:36:08 +0200

    atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _atoi - A function that convert a string to an integer.
+ * @s: Pointer to first element in string
+ *
+ * Return: Integer
+ *
+ */
+int _atoi(char *s)
+{
+	int i, num = 0, sign = 1;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		else if (s[i] >= '0' && s[i] <= '9')
+			num = (num * 10) + (s[i] - '0');
+	}
+
+	return ((num * sign));
+}
+
