297982cd95f23cab6f78c156d2e394e29037cff7
Hazem Abolila <hazemabolila44@gmail.com>
2023-04-16 17:15:50 +0200

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,18 +9,19 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	char *ptr = dest;
+	int i, dlen = 0, slen = 0;
 
-	while (*ptr != '\0')
-	{
-		ptr++;
-	}
-	while (*src != '\0' && n--)
-	{
-		*ptr = *src;
-		src++;
-		ptr++;
-	}
-	*ptr = '\0';
+	for (i = 0; dest[i] != '\0'; i++)
+		dlen++;
+
+	for (i = 0; src[i] != '\0'; i++)
+		slen++;
+
+	if (n >= slen)
+		n = slen;
+
+	for (i = 0; i < n; i++)
+		dest[dlen + i] = src[i];
+
 	return (dest);
 }
