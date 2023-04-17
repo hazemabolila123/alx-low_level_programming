3e4253252e0c4757974f0b5ec46512c6388b5ec8
Hazem Abolila <hazemabolila44@gmail.com>
2023-04-17 16:20:13 +0200

    Header File

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef dog_h
+#define dog_h
+/**
+ * struct dog - User define data type
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/** Functions prototypes **/
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
