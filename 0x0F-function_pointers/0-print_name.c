b798148f7f953ea509e61356b5c8e744df7fad84
Hazem Abolila <hazemabolila44@gmail.com>
2023-04-19 16:46:49 +0200

    Call

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,10 @@
 #include "function_pointers.h"
-/**
- * print_name - A function that calls another one.
- * @name: Function parameter
- * @f: Function parameter
- *
- */
 
+/**
+  * print_name - A function that calls asnother one
+  * @name: Function parameter
+  * @f: Function parameter
+  */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
