afd00d860acc173c840fa76308f225c51da3f26a
Hazem Abolila <hazemabolila44@gmail.com>
2023-04-20 19:58:18 +0200

    printstrings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list countptr;
+	char * tmp;
+
+	va_start(countptr, n);
+
+	for (i = 0; i < n; i++)
+	{
+		tmp = va_arg(countptr, char *);
+		if (tmp != NULL)
+			printf("%s", tmp);
+		else
+			printf("%s", "nil");
+
+		if (separator != NULL && i != (n - 1))
+			printf("%s", separator);
+	}
+	putchar('\n');
+
+	va_end(countptr);
+}
+
