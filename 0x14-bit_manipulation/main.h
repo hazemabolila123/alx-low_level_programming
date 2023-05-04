75913332755dd39867ac9f34c4ab703f164eef0d
Hazem Abolila <hazemabolila44@gmail.com>
2023-05-04 11:39:33 +0300

    Header File

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,8 @@
 #ifndef main_h
 #define main_h
 
+#include <unistd.h> 
+
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
