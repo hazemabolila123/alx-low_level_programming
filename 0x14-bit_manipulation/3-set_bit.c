63e364009ea445419eace4b8bb0da9ae9fad2416
Hazem Abolila <hazemabolila44@gmail.com>
2023-05-04 14:04:26 +0300

    Set to 1

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,9 +11,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > (sizeof(long int) * 8 - 1))
 		return (-1);
-	else
-	{
-		*n |= (1 << index);
-		return (1);
-	}
+
+	*n |= (1 << index);
+	return (1);
 }
